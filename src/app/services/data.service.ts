3b2205cda3bbdc82cd213579626dc65db58bcf4f
Roman Doschich <doschich@dnt-lab.com>
2024-07-04 17:02:09 +0300

    change mock fallback for server down response

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@angular/core';
 import { DefaultService as RestApiService } from 'src/app/swagger-api/api/default.service';
 import { UserBasic } from '../swagger-api';
-import { Observable } from 'rxjs';
+import { Observable, Subject, of } from 'rxjs';
+import { mockUserBasic } from '../components/public/user-profile/mock-users';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private user$: Subject<UserBasic> = new Subject();
+  private users$: Subject<UserBasic[]> = new Subject();
 
   constructor(
     private apiService: RestApiService,
   ) { }
 
   public get users(): Observable<UserBasic[]> {
-    return this.apiService.usersGet();
+    this.apiService.usersGet().subscribe({
+      next: (response: UserBasic[]) => this.users$.next(response),
+      error: (error: HttpErrorResponse) => this.users$.next([mockUserBasic]),
+    });
+    return this.users$.asObservable();
   }
 
   public getUser(userId: string): Observable<UserBasic> {
-    return this.apiService.usersUserIdGet(userId);
+    this.apiService.usersUserIdGet(userId).subscribe({
+      next: (response: UserBasic) => this.user$.next(response),
+      error: (error: HttpErrorResponse) => this.user$.next(mockUserBasic),
+    });
+    return this.user$.asObservable();
   }
 }
